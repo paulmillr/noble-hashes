05ddc95a01417097b88d6e41bdcbd13fc970adb7
Paul Miller <paul@paulmillr.com>
2021-10-23 08:41:27 +0300

    Update benches

--- test/benchmark/hashes.js
+++ test/benchmark/hashes.js
@@ -3,27 +3,26 @@ const { run, mark } = bench; // or bench.mark
 const crypto = require('crypto');
 // Noble
 const { sha256 } = require('../../lib/sha256');
-const { sha384, sha512, sha512_256 } = require('../../lib/sha512');
-const { sha3_256, keccak_256 } = require('../../lib/sha3');
+const { sha384, sha512 } = require('../../lib/sha512');
+const { sha3_256 } = require('../../lib/sha3');
 const { k12, m14 } = require('../../lib/sha3-addons');
-const { blake2s } = require('../../lib/blake2s');
 const { blake2b } = require('../../lib/blake2b');
+const { blake2s } = require('../../lib/blake2s');
 const { blake3 } = require('../../lib/blake3');
-const { hmac } = require('../../lib/hmac');
 const { ripemd160 } = require('../../lib/ripemd160');
-// Others
+const { hmac } = require('../../lib/hmac');
 
+// Others
 const createHash = require('create-hash/browser');
 const createHmac = require('create-hmac/browser');
 const stable256 = require('@stablelib/sha256');
 const stableHmac = require('@stablelib/hmac');
 const fastsha256 = require('fast-sha256').hash;
-const stable384 = require('@stablelib/sha384');
-const stable512 = require('@stablelib/sha512');
-const stable512_256 = require('@stablelib/sha512_256');
+const stable2_384 = require('@stablelib/sha384');
+const stable2_512 = require('@stablelib/sha512');
 const stable3 = require('@stablelib/sha3');
-const stableBlake2s = require('@stablelib/blake2s');
-const stableBlake2b = require('@stablelib/blake2b');
+const stableb2b = require('@stablelib/blake2b');
+const stableb2s = require('@stablelib/blake2s');
 const jssha3 = require('js-sha3');
 
 const wrapBuf = (arrayBuffer) => new Uint8Array(arrayBuffer);
@@ -39,41 +38,32 @@ const HASHES = {
   SHA384: {
     node: (buf) => crypto.createHash('sha384').update(buf).digest(),
     'crypto-browserify': (buf) => createHash('sha384').update(buf).digest(),
-    stablelib: (buf) => stable384.hash(buf),
+    stablelib: (buf) => stable2_384.hash(buf),
     noble: (buf) => sha384(buf),
   },
   SHA512: {
     node: (buf) => crypto.createHash('sha512').update(buf).digest(),
     'crypto-browserify': (buf) => createHash('sha512').update(buf).digest(),
-    stablelib: (buf) => stable512.hash(buf),
+    stablelib: (buf) => stable2_512.hash(buf),
     noble: (buf) => sha512(buf),
   },
-  // 'SHA512-256': {
-  //   node: (buf) => crypto.createHash('sha512-256').update(buf).digest(),
-  //   stablelib: (buf) => new stable512_256.SHA512_256().update(buf).digest(),
-  //   noble: (buf) => sha512_256(buf),
-  // },
-  'SHA3-256 & keccak-256': {
+  'SHA3-256, keccak256, shake256': {
     node: (buf) => crypto.createHash('sha3-256').update(buf).digest(),
     stablelib: (buf) => new stable3.SHA3256().update(buf).digest(),
     'js-sha3': (buf) => wrapBuf(jssha3.sha3_256.create().update(buf).arrayBuffer()),
     noble: (buf) => sha3_256(buf),
   },
-  // 'Keccak-256': {
-  //   'js-sha3': (buf) => wrapBuf(jssha3.keccak_256.create().update(buf).arrayBuffer()),
-  //   noble: (buf) => keccak_256(buf),
-  // },
   Kangaroo12: { noble: (buf) => k12(buf) },
   Marsupilami14: { noble: (buf) => m14(buf) },
-  BLAKE2s: {
-    node: (buf) => crypto.createHash('blake2s256').update(buf).digest(),
-    stablelib: (buf) => new stableBlake2s.BLAKE2s().update(buf).digest(),
-    noble: (buf) => blake2s(buf),
-  },
   BLAKE2b: {
     node: (buf) => crypto.createHash('blake2b512').update(buf).digest(),
-    stablelib: (buf) => new stableBlake2b.BLAKE2b().update(buf).digest(),
+    stablelib: (buf) => new stableb2b.BLAKE2b().update(buf).digest(),
     noble: (buf) => blake2b(buf),
+  },
+  BLAKE2s: {
+    node: (buf) => crypto.createHash('blake2s256').update(buf).digest(),
+    stablelib: (buf) => new stableb2s.BLAKE2s().update(buf).digest(),
+    noble: (buf) => blake2s(buf),
   },
   BLAKE3: {
     noble: (buf) => blake3(buf),
