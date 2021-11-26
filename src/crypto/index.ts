148b2c59ce9a726f78c0fee7aff6c76e41fcae94
Jaco <jacogr@gmail.com>
2021-11-26 18:31:10 +0200

    Flatten crypto exports

--- src/crypto/index.ts
+++
@@ -1,6 +0,0 @@
-export const crypto: { node?: any; web?: any } = (() => {
-	// This path will never be followed
-	throw new Error(
-		'noble-hashes/lib/crypto has no entry-point. Export maps are used.'
-	);
-})();
