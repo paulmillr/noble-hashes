412a108c597fe9e6c393c8021529070d95f500b9
Paul Miller <paul@paulmillr.com>
2023-03-13 23:20:16 +0000

    crypto import: use cryptoBrowser by default, cryptoNode in nodejs

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,6 +1,6 @@
-import * as nodeCrypto from 'crypto';
-
+// Global symbol available in browsers only, node.js 19+, deno and others
+declare const globalThis: Record<string, any> | undefined;
 export const crypto: { node?: any; web?: any } = {
-  node: nodeCrypto,
-  web: undefined,
+  node: undefined,
+  web: typeof globalThis === 'object' && 'crypto' in globalThis ? globalThis.crypto : undefined,
 };
