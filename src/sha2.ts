d3b96a883caa55b597d1aa5f06ceea3448e8d16d
Paul Miller <paul@paulmillr.com>
2023-12-11 22:51:29 +0000

    sha2: remove some aliases

--- src/sha2.ts
+++ src/sha2.ts
@@ -1,7 +1,3 @@
 // Usually you either use sha256, or sha512. We re-export them as sha2 for naming consistency.
-export { sha256, sha224, sha256 as sha2_256, sha224 as sha2_224 } from './sha256.js';
-// prettier-ignore
-export {
-  sha512, sha512_224, sha512_256, sha384,
-  sha512 as sha2_512, sha512_224 as sha2_512_224, sha512_256 as sha2_512_256, sha384 as sha2_384
-} from './sha512.js';
+export { sha256, sha224 } from './sha256.js';
+export { sha512, sha512_224, sha512_256, sha384 } from './sha512.js';
