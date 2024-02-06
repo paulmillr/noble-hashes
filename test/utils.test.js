550c2fc0beecf6adaea2bcb8a5b1fa4b4be18790
Jonathan Albrecht <jonathan.albrecht@ibm.com>
2024-02-06 17:31:51 -0500

    Add some basic tests for the byteSwap* functions

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const { should } = require('micro-should');
 const { optional, integer, gen } = require('./generator');
+const { byteSwap, byteSwapIfBE, byteSwap32, isLE } = require('../utils.js');
 
 // Here goes test for tests...
 should(`Test generator`, () => {
@@ -19,6 +20,36 @@ should(`Test generator`, () => {
       { N: 0, b: 2, c: 5 },
     ]
   );
+});
+
+// Byte swapping
+const BYTESWAP_TEST_CASES = [
+  { in: 0x11223344 | 0, out: 0x44332211 | 0 },
+  { in: 0xffeeddcc | 0, out: 0xccddeeff | 0 },
+  { in: 0xccddeeff | 0, out: 0xffeeddcc | 0 },
+];
+
+should('byteSwap', () => {
+  BYTESWAP_TEST_CASES.forEach(test => {
+    assert.deepStrictEqual(test.out, byteSwap(test.in));
+  });
+});
+
+should('byteSwapIfBE', () => {
+  BYTESWAP_TEST_CASES.forEach(test => {
+    if (isLE) {
+      assert.deepStrictEqual(test.in, byteSwapIfBE(test.in));
+    } else {
+      assert.deepStrictEqual(test.out, byteSwapIfBE(test.in));
+    }
+  });
+});
+
+should('byteSwap32', () => {
+  const input = Uint32Array.of([0x11223344, 0xffeeddcc, 0xccddeeff]);
+  const expected = Uint32Array.of([0x44332211, 0xccddeeff, 0xffeeddcc]);
+  byteSwap32(input);
+  assert.deepStrictEqual(expected, input);
 });
 
 if (require.main === module) should.run();
