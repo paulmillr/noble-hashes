d356a1d4176b7ccb62eb206ebdd973cc19f93985
Paul Miller <paul@paulmillr.com>
2024-11-21 08:04:53 +0000

    test: Add more utils tests. Add fast-check

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,8 +1,9 @@
 const { deepStrictEqual, throws } = require('assert');
+const fc = require('fast-check');
 const { describe, should } = require('micro-should');
 const { optional, integer, gen } = require('./generator');
 const { TYPE_TEST } = require('./utils');
-const { byteSwap, byteSwapIfBE, byteSwap32, isLE, hexToBytes, bytesToHex } = require('../utils');
+const { byteSwap, byteSwapIfBE, byteSwap32, isLE, bytesToHex, concatBytes, hexToBytes } = require('../utils');
 
 describe('utils', () => {
   const staticHexVectors = [
@@ -13,6 +14,7 @@ describe('utils', () => {
   ];
   should('hexToBytes', () => {
     for (let v of staticHexVectors) deepStrictEqual(hexToBytes(v.hex), v.bytes);
+    for (let v of staticHexVectors) deepStrictEqual(hexToBytes(v.hex.toUpperCase()), v.bytes);
     for (let v of TYPE_TEST.hex) {
       throws(() => hexToBytes(v));
     }
@@ -23,6 +25,39 @@ describe('utils', () => {
       throws(() => bytesToHex(v));
     }
   });
+  should('hexToBytes <=> bytesToHex roundtrip', () =>
+    fc.assert(
+      fc.property(fc.hexaString({ minLength: 2, maxLength: 64 }), (hex) => {
+        if (hex.length % 2 !== 0) return;
+        deepStrictEqual(hex, bytesToHex(hexToBytes(hex)));
+        deepStrictEqual(hex, bytesToHex(hexToBytes(hex.toUpperCase())));
+        deepStrictEqual(hexToBytes(hex), Uint8Array.from(Buffer.from(hex, 'hex')));
+      })
+    )
+  );
+  should('concatBytes', () => {
+    const a = 1;
+    const b = 2;
+    const c = 0xff;
+    const aa = Uint8Array.from([a]);
+    const bb = Uint8Array.from([b]);
+    const cc = Uint8Array.from([c]);
+    deepStrictEqual(concatBytes(), new Uint8Array());
+    deepStrictEqual(concatBytes(aa, bb), Uint8Array.from([a, b]));
+    deepStrictEqual(concatBytes(aa, bb, cc), Uint8Array.from([a, b, c]));
+    for (let v of TYPE_TEST.bytes)
+      throws(() => {
+        concatBytes(v);
+      });
+  });
+  should('concatBytes random', () =>
+    fc.assert(
+      fc.property(fc.uint8Array(), fc.uint8Array(), fc.uint8Array(), (a, b, c) => {
+        const expected = Uint8Array.from(Buffer.concat([a, b, c]));
+        deepStrictEqual(concatBytes(a.slice(), b.slice(), c.slice()), expected);
+      })
+    )
+  );
 });
 
 describe('utils etc', () => {
