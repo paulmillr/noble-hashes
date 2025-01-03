5e2c9bd41e90f423320cb31736bbef755672f5d3
Paul Miller <paul@paulmillr.com>
2025-01-03 00:44:51 +0000

    Add docs for root module.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+/**
+ * Audited & minimal JS implementation of hash functions, MACs and KDFs. Check out individual modules.
+ * @module
+ */
 throw new Error('root module cannot be imported: import submodules instead. Check out README');
