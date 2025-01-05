1c66ee9e3893dbe451899a0e4c0f1762648b2681
Paul Miller <paul@paulmillr.com>
2025-01-05 02:17:41 +0000

    Format comments

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@
  * @module
  * @example
 ```js
-import { sha256, sha384, sha512, sha224, sha512_256, sha512_384 } from '@noble/hashes/sha2';
+import {
+  sha256, sha384, sha512, sha224, sha512_256, sha512_384
+} from '@noble/hashes/sha2';
 import {
   sha3_224, sha3_256, sha3_384, sha3_512,
   keccak_224, keccak_256, keccak_384, keccak_512,
