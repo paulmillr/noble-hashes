5a88f6f851c392ed54a68c477fb17a2c7e12da71
Paul Miller <paul@paulmillr.com>
2025-01-18 04:35:29 +0000

    Update micro-should

--- test/index.js
+++ test/index.js
@@ -3,18 +3,17 @@ import { should } from 'micro-should';
 // Generic hash tests
 import { init } from './hashes.test.js';
 // Specific vectors for hash functions if available
-import './keccak.test.js';
 import './blake.test.js';
+import './keccak.test.js';
 // Tests generated from rust
 import './hmac.test.js';
 import './kdf.test.js';
 // import './argon2.test.js';
-import './eskdf.test.js';
 import './async.test.js';
 import './clone.test.js';
+import './eskdf.test.js';
 import './u64.test.js';
 import './utils.test.js';
 
 init();
-should.opts.STOP_AT_ERROR = false;
 should.runWhen(import.meta.url);
