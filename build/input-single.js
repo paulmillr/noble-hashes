4b708a5d7a038e16cd39d0d5fa20da4dc76c9a3a
Paul Miller <paul@paulmillr.com>
2025-01-18 01:32:37 +0000

    Bump jsbt helpers 0.3.0. Add build:release script.

--- build/input-single.js
+++
@@ -1,3 +0,0 @@
-import { bytesToHex, hexToBytes, concatBytes, utf8ToBytes, randomBytes } from '@noble/hashes/utils';
-export { sha256 } from '@noble/hashes/sha256';
-export const utils = { bytesToHex, hexToBytes, concatBytes, utf8ToBytes, randomBytes };
