b81f78838715be9c521e5374cdd0c7632e8d502f
Paul Miller <paul@paulmillr.com>
2025-04-14 11:32:33 +0000

    Fix lint

--- test/misc/scrypt-time.js
+++ test/misc/scrypt-time.js
@@ -8,6 +8,6 @@ for (let i = 16; i < 26; i++) {
   const key = scrypt(pass, salt, { N: N, r: 8, p: 1, dkLen: 32, maxmem: 2 ** 40 });
   const diff = Date.now() - start;
   const sec = (diff / 1000).toFixed(1) + 's';
-  const ram = process.memoryUsage().arrayBuffers / (1024**2);
+  const ram = process.memoryUsage().arrayBuffers / 1024 ** 2;
   console.log(`N=2^${i}`, sec, ram.toFixed(0) + 'MB');
 }
