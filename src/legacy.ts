b944c7c5b823d939e5530b8ebd917c7a0f523e98
Paul Miller <paul@paulmillr.com>
2025-04-19 00:50:18 +0000

    ripemd160: move into legacy

--- src/legacy.ts
+++ src/legacy.ts
@@ -1,22 +1,15 @@
 /**
-SHA1 (RFC 3174) and MD5 (RFC 1321) legacy, broken hash functions.
-Don't use them in a new protocol. What "broken" means:
 
-- Collisions can be made with 2^18 effort in MD5, 2^60 in SHA1.
+SHA1 (RFC 3174), MD5 (RFC 1321) and RIPEMD160 (RFC 2286) legacy, weak hash functions.
+Don't use them in a new protocol. What "weak" means:
+
+- Collisions can be made with 2^18 effort in MD5, 2^60 in SHA1, 2^80 in RIPEMD160.
 - No practical pre-image attacks (only theoretical, 2^123.4)
 - HMAC seems kinda ok: https://datatracker.ietf.org/doc/html/rfc6151
-
-MD5 architecture is similar to SHA1, with some differences:
-
-- reduced output length: 16 bytes (128 bit) instead of 20
-- 64 rounds, instead of 80
-- little-endian: could be faster, but will require more code
-- non-linear index selection: huge speed-up for unroll
-- per round constants: more memory accesses, additional speed-up for unroll
  * @module
  */
 import { Chi, HashMD, Maj } from './_md.ts';
-import { type CHash, rotl, wrapConstructor } from './utils.ts';
+import { type CHash, wrapConstructor as createHash, rotl } from './utils.ts';
 
 /** Initial SHA1 state */
 const SHA1_IV = /* @__PURE__ */ new Uint32Array([
@@ -94,10 +87,10 @@ export class SHA1 extends HashMD<SHA1> {
 }
 
 /** SHA1 (RFC 3174) legacy hash function. It was cryptographically broken. */
-export const sha1: CHash = /* @__PURE__ */ wrapConstructor(() => new SHA1());
+export const sha1: CHash = /* @__PURE__ */ createHash(() => new SHA1());
 
 /** Per-round constants */
-const p32 = 2 ** 32;
+const p32 = /* @__PURE__ */ Math.pow(2, 32);
 const K = /* @__PURE__ */ Array.from({ length: 64 }, (_, i) =>
   Math.floor(p32 * Math.abs(Math.sin(i + 1)))
 );
@@ -172,5 +165,129 @@ export class MD5 extends HashMD<MD5> {
   }
 }
 
-/** MD5 (RFC 1321) legacy hash function. It was cryptographically broken. */
-export const md5: CHash = /* @__PURE__ */ wrapConstructor(() => new MD5());
+/**
+ * MD5 (RFC 1321) legacy hash function. It was cryptographically broken.
+ * MD5 architecture is similar to SHA1, with some differences:
+ * - Reduced output length: 16 bytes (128 bit) instead of 20
+ * - 64 rounds, instead of 80
+ * - Little-endian: could be faster, but will require more code
+ * - Non-linear index selection: huge speed-up for unroll
+ * - Per round constants: more memory accesses, additional speed-up for unroll
+ */
+export const md5: CHash = /* @__PURE__ */ createHash(() => new MD5());
+
+// RIPEMD-160
+
+const Rho160 = /* @__PURE__ */ new Uint8Array([
+  7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8,
+]);
+const Id160 = /* @__PURE__ */ (() => Uint8Array.from(new Array(16).fill(0).map((_, i) => i)))();
+const Pi160 = /* @__PURE__ */ (() => Id160.map((i) => (9 * i + 5) % 16))();
+const idxLR = /* @__PURE__ */ (() => {
+  const L = [Id160];
+  const R = [Pi160];
+  const res = [L, R];
+  for (let i = 0; i < 4; i++) for (let j of res) j.push(j[i].map((k) => Rho160[k]));
+  return res;
+})();
+const idxL = /* @__PURE__ */ (() => idxLR[0])();
+const idxR = /* @__PURE__ */ (() => idxLR[1])();
+// const [idxL, idxR] = idxLR;
+
+const shifts160 = /* @__PURE__ */ [
+  [11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8],
+  [12, 13, 11, 15, 6, 9, 9, 7, 12, 15, 11, 13, 7, 8, 7, 7],
+  [13, 15, 14, 11, 7, 7, 6, 8, 13, 14, 13, 12, 5, 5, 6, 9],
+  [14, 11, 12, 14, 8, 6, 5, 5, 15, 12, 15, 14, 9, 9, 8, 6],
+  [15, 12, 13, 13, 9, 5, 8, 6, 14, 11, 12, 11, 8, 6, 5, 5],
+].map((i) => new Uint8Array(i));
+const shiftsL160 = /* @__PURE__ */ idxL.map((idx, i) => idx.map((j) => shifts160[i][j]));
+const shiftsR160 = /* @__PURE__ */ idxR.map((idx, i) => idx.map((j) => shifts160[i][j]));
+const Kl160 = /* @__PURE__ */ Uint32Array.from([
+  0x00000000, 0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xa953fd4e,
+]);
+const Kr160 = /* @__PURE__ */ Uint32Array.from([
+  0x50a28be6, 0x5c4dd124, 0x6d703ef3, 0x7a6d76e9, 0x00000000,
+]);
+// It's called f() in spec.
+function ripe_f(group: number, x: number, y: number, z: number): number {
+  if (group === 0) return x ^ y ^ z;
+  else if (group === 1) return (x & y) | (~x & z);
+  else if (group === 2) return (x | ~y) ^ z;
+  else if (group === 3) return (x & z) | (y & ~z);
+  else return x ^ (y | ~z);
+}
+// Temporary buffer, not used to store anything between runs
+const BUF_160 = /* @__PURE__ */ new Uint32Array(16);
+export class RIPEMD160 extends HashMD<RIPEMD160> {
+  private h0 = 0x67452301 | 0;
+  private h1 = 0xefcdab89 | 0;
+  private h2 = 0x98badcfe | 0;
+  private h3 = 0x10325476 | 0;
+  private h4 = 0xc3d2e1f0 | 0;
+
+  constructor() {
+    super(64, 20, 8, true);
+  }
+  protected get(): [number, number, number, number, number] {
+    const { h0, h1, h2, h3, h4 } = this;
+    return [h0, h1, h2, h3, h4];
+  }
+  protected set(h0: number, h1: number, h2: number, h3: number, h4: number): void {
+    this.h0 = h0 | 0;
+    this.h1 = h1 | 0;
+    this.h2 = h2 | 0;
+    this.h3 = h3 | 0;
+    this.h4 = h4 | 0;
+  }
+  protected process(view: DataView, offset: number): void {
+    for (let i = 0; i < 16; i++, offset += 4) BUF_160[i] = view.getUint32(offset, true);
+    // prettier-ignore
+    let al = this.h0 | 0, ar = al,
+        bl = this.h1 | 0, br = bl,
+        cl = this.h2 | 0, cr = cl,
+        dl = this.h3 | 0, dr = dl,
+        el = this.h4 | 0, er = el;
+
+    // Instead of iterating 0 to 80, we split it into 5 groups
+    // And use the groups in constants, functions, etc. Much simpler
+    for (let group = 0; group < 5; group++) {
+      const rGroup = 4 - group;
+      const hbl = Kl160[group], hbr = Kr160[group]; // prettier-ignore
+      const rl = idxL[group], rr = idxR[group]; // prettier-ignore
+      const sl = shiftsL160[group], sr = shiftsR160[group]; // prettier-ignore
+      for (let i = 0; i < 16; i++) {
+        const tl = (rotl(al + ripe_f(group, bl, cl, dl) + BUF_160[rl[i]] + hbl, sl[i]) + el) | 0;
+        al = el, el = dl, dl = rotl(cl, 10) | 0, cl = bl, bl = tl; // prettier-ignore
+      }
+      // 2 loops are 10% faster
+      for (let i = 0; i < 16; i++) {
+        const tr = (rotl(ar + ripe_f(rGroup, br, cr, dr) + BUF_160[rr[i]] + hbr, sr[i]) + er) | 0;
+        ar = er, er = dr, dr = rotl(cr, 10) | 0, cr = br, br = tr; // prettier-ignore
+      }
+    }
+    // Add the compressed chunk to the current hash value
+    this.set(
+      (this.h1 + cl + dr) | 0,
+      (this.h2 + dl + er) | 0,
+      (this.h3 + el + ar) | 0,
+      (this.h4 + al + br) | 0,
+      (this.h0 + bl + cr) | 0
+    );
+  }
+  protected roundClean(): void {
+    BUF_160.fill(0);
+  }
+  destroy(): void {
+    this.destroyed = true;
+    this.buffer.fill(0);
+    this.set(0, 0, 0, 0, 0);
+  }
+}
+
+/**
+ * RIPEMD-160 - a legacy hash function from 1990s.
+ * * https://homes.esat.kuleuven.be/~bosselae/ripemd160.html
+ * * https://homes.esat.kuleuven.be/~bosselae/ripemd160/pdf/AB-9601/AB-9601.pdf
+ */
+export const ripemd160: CHash = /* @__PURE__ */ createHash(() => new RIPEMD160());
