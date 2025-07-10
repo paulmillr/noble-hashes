11646eb011c4ca45de7894ed3afc3f98613bf05d
Paul Miller <paul@paulmillr.com>
2025-07-10 00:15:23 +0000

    Adjust single-build output file

--- test/build/input.js
+++ test/build/input.js
@@ -1,15 +1,18 @@
-import { bytesToHex, concatBytes, hexToBytes, randomBytes, utf8ToBytes } from '@noble/hashes/utils';
-export { argon2id } from '@noble/hashes/argon2';
-export { blake224, blake256, blake384, blake512 } from '@noble/hashes/blake1';
-export { blake2b, blake2s } from '@noble/hashes/blake2';
-export { blake3 } from '@noble/hashes/blake3';
-export { eskdf } from '@noble/hashes/eskdf';
-export { hkdf } from '@noble/hashes/hkdf';
-export { hmac } from '@noble/hashes/hmac';
-export { md5, ripemd160, sha1 } from '@noble/hashes/legacy';
-export { pbkdf2, pbkdf2Async } from '@noble/hashes/pbkdf2';
-export { scrypt, scryptAsync } from '@noble/hashes/scrypt';
-export { sha224, sha256, sha384, sha512, sha512_224, sha512_256 } from '@noble/hashes/sha2';
+import { bytesToHex, concatBytes, hexToBytes, randomBytes, utf8ToBytes } from '@noble/hashes/utils.js';
+export { argon2id } from '@noble/hashes/argon2.js';
+export { blake224, blake256, blake384, blake512 } from '@noble/hashes/blake1.js';
+export { blake2b, blake2s } from '@noble/hashes/blake2.js';
+export { blake3 } from '@noble/hashes/blake3.js';
+export { eskdf } from '@noble/hashes/eskdf.js';
+export { hkdf } from '@noble/hashes/hkdf.js';
+export { hmac } from '@noble/hashes/hmac.js';
+export { md5, ripemd160, sha1 } from '@noble/hashes/legacy.js';
+export { pbkdf2, pbkdf2Async } from '@noble/hashes/pbkdf2.js';
+export { scrypt, scryptAsync } from '@noble/hashes/scrypt.js';
+export { sha224, sha256, sha384, sha512, sha512_224, sha512_256 } from '@noble/hashes/sha2.js';
+export {
+  cshake128, cshake256, keccakprg, kmac128, kmac256, kt128, kt256, turboshake128, turboshake256
+} from '@noble/hashes/sha3-addons.js';
 export {
   keccak_224,
   keccak_256,
@@ -19,10 +22,7 @@ export {
   sha3_384,
   sha3_512, shake128,
   shake256
-} from '@noble/hashes/sha3';
-export {
-  cshake128, cshake256, k12, keccakprg, kmac128, kmac256, m14, turboshake128, turboshake256
-} from '@noble/hashes/sha3-addons';
+} from '@noble/hashes/sha3.js';
 
 export const utils = { bytesToHex, hexToBytes, concatBytes, utf8ToBytes, randomBytes };
 
